0d63c79ab6f8909b8ef962fa868f11730df77ba5
Akin909 <akin.sowemimo@gmail.com>
2017-07-03 02:27:49 +0100

    further attempts to tame the beast that is clojure

--- src/obligatory_todo/core.cljs
+++ src/obligatory_todo/core.cljs
@@ -16,29 +16,71 @@
                    :margin "5px 0"
                    :padding "5px"})
 
-(defonce todos (atom (map {})))
-(defonce current-todo (atom ""))
+(def button-styles {:font-size "20px"
+                    :width "100px"
+                    :height "50px"
+                    :box-shadow "0 1px 0 rgba(0, 0, 0, 0.5)"
+                    :margin "5px"
+                    :border-radius "5px"
+                    :border "none" :padding "5px"})
 
-(defn todo-input [placeholder]
+(defonce todos (atom (sorted-map)))
+(defonce current-todo (atom 0))
+
+(defn create-todo [value]
+  (let [id ( swap! current-todo inc)]
+  (swap! todos assoc {:id id :title value :completed false  })))
+
+(defonce init (do
+    (create-todo "Do a thing")
+    ;(create-todo "Stuff is Happening")
+    ))
+
+(prn todos)
+(defn todo-input
+  "An input element which updates its value on change"
+  [type value placeholder]
   [:input
-   {:type "text"
+   {:type type
     :style input-styles
     :placeholder placeholder
-    :value @current-todo
-    :on-change #(reset! current-todo (-> % .-target .-value ))}])
+    :value @value
+    :on-change #(reset! value (-> % .-target .-value ))
+    ;:on-blur (if-not (empty? value) (create-todo @value) )
+    }
+   (prn todos)])
 
+(defn save-button []
+  [:button
+   {:on-click ()
+    :style button-styles}
+   "Save"])
 
-(defn add-todo []
+(defn add-todo [value]
   [:div {:style {:display "flex" :flex-direction "column"}}
-   [todo-input "Add A Todo"]
-   [todo-input "Add A Title"]])
+   [todo-input "text" value "Add A Todo"]
+   ])
+
+(defn current-todos [items]
+  (prn "items" items)
+  [:ul
+   (for [item items]
+    ^{:key item} [:li "Todo: " item])
+   ])
 
 (defn todo-container []
-  [:center
-   {:style {:font-family "Helvetica"}}
-   [:h1
-    "Obligatory Todo"]
-   [add-todo]])
+  (let [title (atom nil)]
+    (fn []
+       [:center
+       {:style {:font-family "Helvetica"}}
+       [:h1
+        "Obligatory Todo"]
+       [add-todo title]
+       (let [x (vals @todos)]
+        (prn "vals" x)
+        [current-todos x])
+       ;[save-button]
+       ])))
 
 (reagent/render-component [todo-container]
                           (. js/document (getElementById "app")))
